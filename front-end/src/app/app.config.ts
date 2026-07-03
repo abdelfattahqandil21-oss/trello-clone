aa9306cb0b8ed96b9de6117dd5fb83c116892764
Abdelfattah Qandil <abdelfattahqandil21@gmail.com>
2026-07-03 15:51:53 +0300

    build: configure core application routing, styles, and providers

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,13 +1,28 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { loaderInterceptor } from './core/interceptors/loader.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient()
-  ]
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withViewTransitions(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      }),
+    ),
+    provideHttpClient(withInterceptors([errorInterceptor, loaderInterceptor])),
+  ],
 };
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'boards', pathMatch: 'full' },
+
+  // Public routes (no shell)
+  { path: 'login', loadComponent: () => import('./public/login/login').then((m) => m.Login) },
+  {
+    path: 'register',
+    loadComponent: () => import('./public/register/register').then((m) => m.Register),
+  },
+
+  // App routes (inside shell)
+  {
+    path: '',
+    loadComponent: () => import('./layout/shell').then((m) => m.Shell),
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/boardParent/board.routes'),
+      },
+    ],
+  },
+];
