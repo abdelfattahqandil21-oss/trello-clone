e8df518b6615f404ef2dec3eda1173759bf3a0c6
My Computer <My Computer@DESKTOP-JA1HPIO>
2026-07-07 13:27:01 +0300

    feature: Complete BackEnd Structure

---
+++ back-end/TrelloClone/Areas/Chat/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrelloClone.Areas.Chat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
---
+++ back-end/TrelloClone/Areas/Identity/Controllers/AccountsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrelloClone.Areas.Identity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+    }
+}
